cf029b68511bbdd14787c1f5914f4b52ba01b0d1
Joël <mail@joelbosch.nl>
2024-06-29 22:22:20 +0200

    Create new scrollable chronology select

--- client/src/lib/components/Chronology.svelte
+++ client/src/lib/components/Chronology.svelte
@@ -2,6 +2,7 @@
   import type { WonTrack } from '$lib/types/WonTrack';
   import type { Artist } from '$lib/types/Artist';
   import type { Answer } from '$lib/types/Answer';
+	import { onMount } from 'svelte';
 
   export let disabled: boolean = false;
   export let wonTracks: WonTrack[] = [];
@@ -11,17 +12,16 @@
     releaseYear: string;
     name?: string;
     artists?: string;
+    isGuess?: boolean;
   }
 
+  let container: HTMLOListElement;
+
+  const UNKNOWN_RELEASE_YEAR = "???";
   const guessCard: Card = {
-    releaseYear: "???",
+    releaseYear: UNKNOWN_RELEASE_YEAR,
+    isGuess: true,
   }
-
-  const trackToCard = ({ track }: WonTrack) : Card => ({
-    releaseYear: track.releaseYear?.toString() ?? '???',
-    name: track.name,
-    artists: joinArtists(track.artists),
-  });
 
   const joinArtists = (artists: Artist[] = []) => {
     return artists.map((artist) => artist.name).join(', ');
@@ -39,26 +39,42 @@
     });
   };
 
+  const trackToCard = ({ track }: WonTrack) : Card => ({
+    releaseYear: track.releaseYear?.toString() ?? UNKNOWN_RELEASE_YEAR,
+    name: track.name,
+    artists: joinArtists(track.artists),
+  });
+
   const sortWonTracks = (a: WonTrack, b: WonTrack) => {
     const aReleaseYear = a.track.releaseYear ?? 0;
     const bReleaseYear = b.track.releaseYear ?? 0;
-    return bReleaseYear + aReleaseYear;
+    return aReleaseYear - bReleaseYear;
+  };
+
+  const onWheelEvent = (event: WheelEvent) => {
+    const direction = event.deltaY + event.deltaX > 0 ? 1 : -1;
+
+    guessIndex = Math.min(Math.max(0, guessIndex + direction), trackCards.length);
   };
 
   let trackCards: Card[];
   $: trackCards = wonTracks.toSorted(sortWonTracks).map(trackToCard);
 
   let guessIndex: number;
-  $: guessIndex = Math.ceil(wonTracks.length / 2);
+  $: guessIndex = Math.ceil(trackCards.length / 2);
 
   let cards: Card[];
-  $: cards = [...trackCards.slice(0, guessIndex), ...trackCards.slice(0, guessIndex), guessCard, ...trackCards.slice(guessIndex), ...trackCards.slice(guessIndex)];
+  $: cards = [...trackCards.slice(0, guessIndex), guessCard, ...trackCards.slice(guessIndex)];
   $: cards, selectAnswer();
+
+  onMount(() => {
+    container.addEventListener('wheel', onWheelEvent);
+  });
 </script>
 
-<ol class="chronology" class:disabled={disabled}>
+<ol class="chronology" class:disabled={disabled} bind:this="{container}">
   {#each cards as card, cardIndex}
-    <li class="card {card.releaseYear === '???' ? 'guess' : ''}" style="--normalized-index: {cardIndex - 2}">
+    <li class="card {card.isGuess ? 'guess' : ''}" style="--normalized-index: {cardIndex - guessIndex}">
       <h2>{card.releaseYear}</h2>
 
       {#if card.name}
@@ -74,10 +90,12 @@
 
 <style lang="scss">
   .chronology {
+    container-type: size;
+    container-name: chronology;
     position: relative;
     width: 100%;
     flex-grow: 1;
-    padding: 1rem;
+    padding: 8rem;
     box-sizing: border-box;
     overflow: hidden;
 
@@ -85,20 +103,60 @@
       --card-border-radius: 1rem;
       --normalized-index: 0;
       --centered-index: max(var(--normalized-index), -1 * var(--normalized-index));
+      --aspect-ratio: 1 / 1.25;
+      --default-vertical-distance: 32cqh;
+      --default-horizontal-distance: 35cqw;
+      --distance-increase: -0.125;
 
-      opacity: min(1, max(0, 1.5 - var(--centered-index) * 0.5));
+      @container chronology (min-height: 20rem) {
+        --default-vertical-distance: 6rem;
+      }
+      @container chronology (min-width: 20rem) {
+        --default-horizontal-distance: 7rem;
+      }
+
+      display: flex;
+      box-sizing: border-box;
+      container-type: size;
+      container-name: card;
+      height: 16rem;
       aspect-ratio: 1 / 1.25;
-      height: calc(16rem - (var(--centered-index) * 3rem));
+      z-index: calc(var(--card-level) - var(--centered-index));
 
       position: absolute;
       top: 50%;
       left: 50%;
-      transform: translate(-50%, -50%);
+      transform:
+        translateX(
+          calc(
+            -50% 
+            + max(
+              min(
+                var(--normalized-index) * var(--default-horizontal-distance)
+                - var(--distance-increase) * var(--centered-index) * var(--normalized-index) * var(--default-horizontal-distance),
+                2 * var(--default-horizontal-distance)
+              ),
+              -2 * var(--default-horizontal-distance)
+            )
+          )
+        )
+        translateY(
+          calc(
+            -50%
+            - max(
+              min(
+                var(--normalized-index) * var(--default-vertical-distance)
+                - var(--distance-increase) * var(--centered-index) * var(--normalized-index) * var(--default-vertical-distance),
+                2 * var(--default-vertical-distance)
+              ),
+              -2 * var(--default-vertical-distance)
+            )
+          )
+        )
+        scale(max(calc(1 - var(--centered-index) * 0.2), 0)
+      );
       
-      container-type: size;
       list-style: none;
-      display: flex;
-      box-sizing: border-box;
       flex-direction: column;
       justify-content: center;
       align-items: center;
@@ -109,19 +167,26 @@
       background-color: var(--white);
       overflow: hidden;
 
+      transition:
+        transform var(--animation-speed-quick),
+        z-index var(--animation-speed-quick),
+        opacity var(--animation-speed-quick),
+        border var(--animation-speed-quick),
+        color var(--animation-speed-quick);
+
       .track {
-        font-size: 16cqmin;
+        font-size: 2em;
       }
 
       p {
         display: -webkit-box;
         -webkit-line-clamp: 2;
         -webkit-box-orient: vertical;
-        font-size: 12cqmin;
+        font-size: 1.25em;
       }
 
       h2 {
-        font-size: 45cqmin;
+        font-size: 5em;
       }
 
       p, h2 {
