be4b0a3c5a5406b8388137b439585c681110d842
Joël <mail@joelbosch.nl>
2024-07-07 13:54:17 +0200

    Horizontal scaling also works

--- client/src/lib/components/Chronology.svelte
+++ client/src/lib/components/Chronology.svelte
@@ -172,6 +172,7 @@
 
       $distance-from-guess-card: max(var(--normalized-index), -1 * var(--normalized-index));
       $card-height: clamp(var(--card-min-height), var(--card-scale-height), var(--card-max-height));
+      $card-width: calc($card-height * var(--card-aspect-ratio));
 
       display: flex;
       border: 1px solid deeppink;
@@ -194,30 +195,27 @@
       $scale: clamp(0, $clamped-fraction, 1);
       $index-offset: calc($scale - 1);
       $card-offset: calc($index-offset * 50 * $direction);
-      $card-vertical-offset: calc($card-offset * 1cqh - $direction * $card-height * 0.33);
-      $card-horizontal-offset: calc($card-offset * 1cqw);
 
       $size-scale: calc($scale * 0.5 + 0.5);
       $reverse-size-scale: calc(1 - $size-scale);
-      $card-scaled-height: calc($card-height * $size-scale);
-      $card-scaled-width: calc($card-scaled-height * var(--card-aspect-ratio));
-      $card-scaling-height-loss: calc($reverse-size-scale * $card-height);
 
+      $card-vertical-offset: calc($card-offset * 1cqh - $direction * $card-height * 0.33);
+      $card-scaled-height: calc($card-height * $size-scale);
+      $card-scaling-height-loss: calc($reverse-size-scale * $card-height);
       $card-at-top: calc(0% - $card-scaling-height-loss * 0.5);
-      $card-at-vertical-center: calc(50cqh - $card-scaled-height * 0.5 - $card-scaling-height-loss * 0.5);
+      $card-at-vertical-center: calc(50cqh - ($card-scaled-height + $card-scaling-height-loss) * 0.5);
       $card-at-bottom: calc(100cqh - $card-height + $card-scaling-height-loss * 0.5);
+      $vertical-transform: translateY(clamp($card-at-top, calc($card-at-vertical-center + $card-vertical-offset), $card-at-bottom));
       
-      $card-at-horizontal-center: calc(50cqw - $card-scaled-width / 2);
-      $card-at-right: calc(100cqw - $card-scaled-width);
-      transform:
-        translateY(
-          clamp(
-            $card-at-top,
-            calc($card-at-vertical-center + $card-vertical-offset),
-            $card-at-bottom
-          )
-        )
-        scale($size-scale);
+      $card-horizontal-offset: calc($card-offset * 1cqw - $direction * $card-width * 0.33);
+      $card-scaled-width: calc($card-width * $size-scale);
+      $card-scaling-width-loss: calc($reverse-size-scale * $card-width);
+      $card-at-left: calc(0% - $card-scaling-width-loss * 0.5);
+      $card-at-horizontal-center: calc(50cqw - ($card-scaled-width + $card-scaling-width-loss) * 0.5);
+      $card-at-right: calc(100cqw - $card-width + $card-scaling-width-loss * 0.5);
+      $horizontal-transform: translateX(clamp($card-at-left, calc($card-at-horizontal-center - $card-horizontal-offset), $card-at-right));
+
+      transform: $vertical-transform $horizontal-transform scale($size-scale);
       
       list-style: none;
       flex-direction: column;
