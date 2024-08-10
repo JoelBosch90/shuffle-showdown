1886ef86f84fcdac07b9ae5db2cfbc44fdfa6470
Joël <mail@joelbosch.nl>
2024-08-10 20:29:49 +0200

    Enable verification in all 4 languages

--- api/lib/wikipedia/languages/Map.go
+++ api/lib/wikipedia/languages/Map.go
@@ -3,11 +3,14 @@ package languages
 import (
 	"api/lib/wikipedia/languages/models"
 	"api/lib/wikipedia/languages/packs/dutch"
+	"api/lib/wikipedia/languages/packs/english"
+	"api/lib/wikipedia/languages/packs/french"
+	"api/lib/wikipedia/languages/packs/german"
 )
 
 var Map = map[models.Language]models.Pack{
-	// models.English: english.Pack,
-	models.Dutch: dutch.Pack,
-	// models.German: german.Pack,
-	// models.French: french.Pack,
+	models.English: english.Pack,
+	models.Dutch:   dutch.Pack,
+	models.German:  german.Pack,
+	models.French:  french.Pack,
 }
